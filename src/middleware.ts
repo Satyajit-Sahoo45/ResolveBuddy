10af128d43e2680d3426bc96f341920235105c0f
Satyajit-Sahoo45 <sahoosatyajit2801@gmail.com>
2024-05-17 00:09:00 +0530

    modify middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,4 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   secret: process.env.NEXT_AUTH_SECRET,
 });
-
 export const config = { matcher: ["/your-rooms", "/dashboard"] };
